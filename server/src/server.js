5798172f17e40fc838a1bb11243f4fc2b92d2dbe
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-03 14:07:42 +0100

    fix: server.js commented and url usage fix

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,29 @@
+// express eases the use of the server and its communication
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 // stationRoutes import
 import stationRoutes from './routes/stationRoutes.js';
-
+// reads the .env file to read variables
 dotenv.config();
-
 const app = express();
 
 // Middlewares
-app.use(cors()); // Enables React  (port  5173) to connect to the server (port 3000)
-app.use(express.json()); // The servere will understand JSON
-app.use('/api', stationRoutes);
+
+// enables React to communicate with the servere with no blocking threat
+// enables React  (port  5173) to connect to the server (port 3000)
+app.use(cors()); 
+// the servere will understand JSON
+app.use(express.json());
+// every query with /api/stations will be handled by stationRoutes.
+app.use('/api/stations', stationRoutes);
 
 //  test path
 app.get('/', (req, res) => {
   res.send('API de Estaciones de Tren funcionando');
 });
 
+// the server listens to the port 3000, it there is no .env info, it uses 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
