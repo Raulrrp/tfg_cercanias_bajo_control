5ec3fc016f68766abb59eb80a09939d7131d196d
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-09 11:52:27 +0100

    feat: stationService

---
+++ server/src/services/stationService.js
@@ -0,0 +1,14 @@
+import { fetchAllStationsFromFile } from '../data/files/stationRepo.js';
+import { Station } from '@tfg_cercanias_bajo_control/common/models/Station.js';
+
+export const getCercaniasStations = async () => {
+  const data = await fetchAllStationsFromFile();
+  
+  if (!data || !data.records) return [];
+
+  return data.records
+    // 1. Filter directly on the raw array (index 10 is 'CERCANIAS')
+    .filter(record => record[10] === 'SI')
+    // 2. Map only the matches to our clean Station objects
+    .map(record => new Station(record));
+};
