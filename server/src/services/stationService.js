c384426ed949bb74c1edffddd6b4ffa09619811a
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-10 13:33:52 +0100

    docs: comment fix in stationService.js

--- server/src/services/stationService.js
+++ server/src/services/stationService.js
@@ -1,4 +1,3 @@
-// this needs decoupling
 // additionally "cercanias" == SI is not a good criterium
 import { fetchStations } from '../data/files/stationRepo.js';
 import { Station } from '@tfg_cercanias_bajo_control/common/models/Station.js';
