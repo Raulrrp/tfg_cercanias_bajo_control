c8c60e27126e61c908c58aeec52a7f67db52956c
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-10 13:33:16 +0100

    Decoupling of Station.js, stationRepo.js and stationService.js

--- server/src/data/files/stationRepo.js
+++ server/src/data/files/stationRepo.js
@@ -1,3 +1,4 @@
+import { StationMapper } from '../mappers/StationMapper.js';
 import fs from 'fs/promises';
 import path from 'path';
 
@@ -8,12 +9,17 @@ const STATIONS_FILE_PATH = path.join(process.cwd(), 'data_files', 'stations.json
  * Reads the stations.json file from the local filesystem
  * @returns {Promise<Array>} List of all stations
  */
-export const fetchAllStationsFromFile = async () => {
+export const fetchStations = async () => {
   try {
     // utf-8 indicates that the content is text-formatted, returns text also.
     const data = await fs.readFile(STATIONS_FILE_PATH, 'utf-8');
+
+    const objetoCompleto = JSON.parse(data);
+    const records = objetoCompleto.records;
     // reads a text json formatted and returns a JS Object
-    return JSON.parse(data);
+    return records
+    .filter(record => record[10] === 'SI')
+    .map(StationMapper.toDomain);
   } catch (error) {
     // Log technical error and throw a readable message
     console.error(`[FileSystem Error]: Failed to read file at ${STATIONS_FILE_PATH}`, error);
