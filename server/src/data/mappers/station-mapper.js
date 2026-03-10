6e9ecc1031cd71399d5b9c16b9c58cba6887e071
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-10 18:08:12 +0100

    fix: station-repo and station-mapper

--- server/src/data/mappers/station-mapper.js
+++ server/src/data/mappers/station-mapper.js
@@ -1,17 +1,24 @@
 import { Station } from '@tfg_cercanias_bajo_control/common/models/Station.js';
 
 export class StationMapper {
-  // Aquí vive el conocimiento del "record[10] === 'SI'"
+  static #counter = 0;
   static toDomain(record) {
+    this.#counter++;
     return new Station({
-      id: record[0],
-      code: parseInt(record[1], 10),
-      name: record[2],
-      latitude: parseFloat(record[3]),
-      longitude: parseFloat(record[4]),
-      address: record[5],
-      zipcode: parseInt(record[6], 10),
-      city: record[7]
+      id: this.#counter,
+      code: parseInt(record[0], 10),
+      name: record[1],
+      latitude: parseFloat(record[2]),
+      longitude: parseFloat(record[3]),
+      address: record[4],
+      zipcode: parseInt(record[5], 10),
+      city: record[6],
+      province: record[7]
     });
+  }
+
+  // resets the counter
+  static resetCounter(){
+    this.#counter = 0;
   }
 }
