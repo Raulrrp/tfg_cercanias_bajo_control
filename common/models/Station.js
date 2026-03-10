616f15591fb32c4a380edfb7129a89df5b953f9b
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-10 13:33:16 +0100

    Decoupling of Station.js, stationRepo.js and stationService.js

--- common/models/Station.js
+++ common/models/Station.js
@@ -1,15 +1,12 @@
-// This needs decoupling
-export class Station {
-  constructor(record) {
-    if (!record) return;
-
-    this.id = record[0];
-    this.code = parseInt(record[1], 10);
-    this.name = record[2];
-    this.latitude = parseFloat(record[3]);
-    this.longitude = parseFloat(record[4]);
-    this.address = record[5];
-    this.zipcode = parseInt(record[6], 10);
-    this.city = record[7];
+export class Station{
+  constructor({id, code, name, latitude, longitude, address, zipcode, city}){
+    this.id = id;
+    this.code = code;
+    this.name = name;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.address = address;
+    this.zipcode = zipcode;
+    this.city = city;
   }
 }
--- server/src/services/stationService.js
+++ server/src/services/stationService.js
@@ -1,16 +1,9 @@
 // this needs decoupling
 // additionally "cercanias" == SI is not a good criterium
-import { fetchAllStationsFromFile } from '../data/files/stationRepo.js';
+import { fetchStations } from '../data/files/stationRepo.js';
 import { Station } from '@tfg_cercanias_bajo_control/common/models/Station.js';
 
 export const getCercaniasStations = async () => {
-  const data = await fetchAllStationsFromFile();
-  
-  if (!data || !data.records) return [];
-
-  return data.records
-    // 1. Filter directly on the raw array (index 10 is 'CERCANIAS')
-    .filter(record => record[10] === 'SI')
-    // 2. Map only the matches to our clean Station objects
-    .map(record => new Station(record));
+  const data = await fetchStations();
+  return data;
 };
