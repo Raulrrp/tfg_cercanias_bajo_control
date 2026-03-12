bb42b5021b90561d8652263d8be7056fe1ed26c7
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-12 16:42:23 +0100

    feat: add station markers in MapView.jsx

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -1,9 +1,15 @@
 import { MapContainer, TileLayer, Polyline, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+// hooks & components
+import { useStations } from '../hooks/station-hook.js';
+import StationMarker from './StationMarker.jsx';
+
 const MapView = () => {
   // Aproximate coordinates of the center of Spain
   const position = [40.4167, -3.7037];
+
+  const { stations, error } = useStations();
 
   return (
     <MapContainer 
@@ -17,7 +23,15 @@ const MapView = () => {
         attribution='&copy; OpenStreetMap contributors'
       />
       <ZoomControl position="topright" />
-      
+
+      {/* Optionally display an error message on the map */}
+      {error && <div className="map-error">{error}</div>}
+
+      {/* render a marker for each station */}
+      {stations.map(st => (
+        <StationMarker key={st.id} station={st} />
+      ))}
+
       {/* Map here your train lines from a GeoJson or service */}
       {/* <Polyline positions={lineData} color="red" /> */}
     </MapContainer>
