523c64bce3617a74dc1ba6cc05a42e3cad99092e
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-12 16:06:09 +0100

    fix: deleted unused import in statino-service.js

--- server/src/services/station-service.js
+++ server/src/services/station-service.js
@@ -1,5 +1,4 @@
 import { fetchStations } from '../data/files/station-repo.js';
-import { Station } from '@tfg_cercanias_bajo_control/common/models/Station.js';
 
 export const getCercaniasStations = async () => {
   const data = await fetchStations();
