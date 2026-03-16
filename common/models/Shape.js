6dbd8f78a03e950cf71d89e9ab34bc8d8c08b731
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-16 23:07:12 +0100

    fix: add shapePoints as constructor parameter and add import

--- common/models/Shape.js
+++ common/models/Shape.js
@@ -1,14 +1,16 @@
+import {ShapePoint} from "@tfg_cercanias_bajo_control/common/models/ShapePoint.js";
+
 export class Shape{
 
     // you can give the fist point or not
-    constructor({id, shapePoint}){
+    constructor({id, shapePoints}){
         this.id = id;
         // If it's already an array, keep it. If it's a single item, put it in an array.
         // If it's missing, make it an empty array.
-        if (!shapePoint) {
+        if (!shapePoints) {
             this.shapePoints = [];
         } else {
-            this.shapePoints = Array.isArray(shapePoint) ? shapePoint : [shapePoint];
+            this.shapePoints = Array.isArray(shapePoints) ? shapePoints : [shapePoints];
         }
     }
     static fromJson(json) {
