9b74633e4d33292527c2b8c67d09250decec0eff
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-17 13:49:28 +0100

    feat: add TrainPos.js

---
+++ common/models/TrainPos.js
@@ -0,0 +1,37 @@
+// whatch out, this json is not the same as the one in the api
+export class TrainPos {
+  constructor({ id, train, trip, latitude, longitude, status, timestamp, nextStop}) {
+    this.id = id;
+    this.train = train;
+    this.trip = trip;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.status = status;
+    this.timestamp = timestamp;
+    this.nextStop = nextStop;
+  }
+  static fromJson(json){
+    return new TrainPos({
+      id: json.id,
+      train: json.train,
+      trip: json.trip,
+      latitude: json.latitude,
+      longitude: json.longitude,
+      status: json.status,
+      timestamp: json.timestamp,
+      nextStop: json.nextStop
+      });
+    }
+  toJson(){
+    return{
+      id: this.id,
+      train: this.train,
+      trip: this.trip,
+      latitude: this.latitude,
+      longitude: this.longitude,
+      status: this.status,
+      timestamp: this.timestamp,
+      nextStop: this.nextStop
+    };
+  }
+}
