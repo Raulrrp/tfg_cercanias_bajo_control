d0c0ce1cd94a91c14875d9a06b9a713f74764149
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-22 20:18:29 +0100

    feat: add update-repo.js

---
+++ server/src/data/remote/update-repo.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { Update } from '@tfg_cercanias_bajo_control/common/models/Update.js';
+
+const RENFE_UPDATES_URL = 'https://gtfsrt.renfe.com/trip_updates.json';
+
+export const fetchUpdates = async () => {
+    try {
+        const response = await axios.get(RENFE_UPDATES_URL);
+        const data = response.data;
+
+        if (!data.entity) return [];
+
+        // We chain .map and .filter directly
+        const jsonUpdates =  data.entity
+            .map(update => {
+                const tripUpdate = update.tripUpdate;
+                if (!tripUpdate) return null;
+
+                const validStop = tripUpdate.stopTimeUpdate?.find(stu => stu.arrival);
+                if (!validStop) return null;
+
+                // Return the instance directly
+                return Update.fromJson({
+                    id: update.id,
+                    trip_id: tripUpdate.trip.tripId,
+                    scheduledState: tripUpdate.trip.scheduleRelationship,
+                    scheduledTime: validStop.arrival.time,
+                    delay: tripUpdate.delay,
+                    nextStopId: validStop.stopId
+                });
+            })
+            .filter(u => u !== null); // Now the nulls are actually removed
+            return jsonUpdates;
+
+    } catch (error) {
+        console.error("Error fetching Renfe updates:", error);
+        throw error;
+    }
+};
