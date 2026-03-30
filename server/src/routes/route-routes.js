45016433a99b060eecf5e4179333d84913bd9e12
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-03-31 00:16:11 +0200

    feat: add route-routes.js

---
+++ server/src/routes/route-routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getRoutes } from '../controllers/route-controller.js';
+
+const router = express.Router();
+// every get queery to /api/routes will run getRoutes
+router.get('/', getRoutes);
+
+export default router;
