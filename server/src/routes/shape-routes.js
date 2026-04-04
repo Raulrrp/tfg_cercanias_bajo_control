e5297466ed538f75f17df023c7af38a5fac420c6
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-04-04 10:05:29 +0200

    feat: add route-shapes path in shape-routes.js

--- server/src/routes/shape-routes.js
+++ server/src/routes/shape-routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {getShapes} from '../controllers/shape-controller.js';
+import { getShapes, getRouteShapes } from '../controllers/shape-controller.js';
 
 const router = express.Router();
 // every get queery to /api/station will run getShapes
 router.get('/', getShapes);
+router.get('/route-shapes', getRouteShapes);
 
 export default router;
