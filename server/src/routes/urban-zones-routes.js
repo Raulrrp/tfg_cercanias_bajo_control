a95c73a78c506fa7a18c6c95d6365beed674a46b
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-04-19 19:27:14 +0200

    fix: remove getUrbanZoneById in urban-zones-controller.js

--- server/src/routes/urban-zones-routes.js
+++ server/src/routes/urban-zones-routes.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import { getUrbanZones, getUrbanZoneById } from '../controllers/urban-zones-controller.js';
+import { getUrbanZones } from '../controllers/urban-zones-controller.js';
 
 const router = express.Router();
 
 // GET all urban zones
 router.get('/', getUrbanZones);
-
-// GET a specific urban zone by id
-router.get('/:id', getUrbanZoneById);
 
 export default router;
