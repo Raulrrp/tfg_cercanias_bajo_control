6e10189363c395c6009df2ad50b2d4deb399e5de
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-05-09 11:34:44 +0200

    fix: only getTrains in train-service

--- server/src/services/train-service.js
+++ server/src/services/train-service.js
@@ -2,27 +2,7 @@ import * as TrainRepo from '../data/remote/train-repo.js';
 
 let ioInstance = null;
 
-// Initializes the tracking process
-export const initTrainTracking = (io) => {
-  ioInstance = io;
-  
-  // First immediate load
-  TrainRepo.fetchTrains().then(trains => {
-    ioInstance.emit('trains_update', trains.map(t => t.toJson()));
-  });
-
-  // 20 seconds loop
-  setInterval(async () => {
-    const trains = await TrainRepo.fetchTrains();
-    
-    // Send to all clients via WebSocket
-    if (ioInstance) {
-      ioInstance.emit('trains_update', trains.map(t => t.toJson()));
-    }
-  }, 20000);
-};
-
-export const getLiveTrains = async () => {
+export const getTrains = async () => {
   // We try to get fresh data or fallback to cache
   const trains = await TrainRepo.fetchTrains();
   return trains;
