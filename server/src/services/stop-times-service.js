9a4e3e96967f8b712f414f292982f8b52265be76
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-05-26 12:56:44 +0200

    feat: add getStopTimeByTripIdAndStopId in stop-times-service

--- server/src/services/stop-times-service.js
+++ server/src/services/stop-times-service.js
@@ -1,4 +1,4 @@
-import { fetchStopTimes, fetchStopTimesByStopId } from '../data/files/stop-times-repo.js';
+import { fetchStopTimes, fetchStopTimesByStopId, fetchStopTimeByTripIdAndStopId } from '../data/files/stop-times-repo.js';
 
 // Get all stop times
 export const getStopTimes = async () => {
@@ -11,4 +11,8 @@ export const getStopTimes = async () => {
 export const getStopTimesByStopId = async (stopId) => {
   const data = await fetchStopTimesByStopId(stopId);
   return data;
+};
+
+export const getStopTimeByTripIdAndStopId = async (tripId, stopId) => {
+  return await fetchStopTimeByTripIdAndStopId(tripId, stopId);
 };
