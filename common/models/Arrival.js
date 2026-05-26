6b6ae7cc918b267e1d99bf58a633d9aafe168584
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-05-26 12:58:56 +0200

    fix: remove last_station attribute in Arrival.js

--- common/models/Arrival.js
+++ common/models/Arrival.js
@@ -1,10 +1,9 @@
 export class Arrival{
-    constructor({train_id, trip_id, line_id, urban_zone_id, last_station, current_station, scheduled_arrival, delay_seconds}){
+    constructor({train_id, trip_id, line_id, urban_zone_id, current_station, scheduled_arrival, delay_seconds}){
         this.train_id = train_id;
         this.trip_id = trip_id;
         this.line_id = line_id;
         this.urban_zone_id = urban_zone_id;
-        this.last_station = last_station;
         this.current_station = current_station;
         this.scheduled_arrival = scheduled_arrival;
         this.delay_seconds = delay_seconds;
@@ -15,7 +14,6 @@ export class Arrival{
             trip_id: json.trip_id,
             line_id: json.line_id,
             urban_zone_id: json.urban_zone_id,
-            last_station: json.last_station,
             current_station: json.current_station,
             scheduled_arrival: json.scheduled_arrival,
             delay_seconds: json.delay_seconds
@@ -27,7 +25,6 @@ export class Arrival{
             trip_id: this.trip_id,
             line_id: this.line_id,
             urban_zone_id: this.urban_zone_id,
-            last_station: this.last_station,
             current_station: this.current_station,
             scheduled_arrival: this.scheduled_arrival,
             delay_seconds: this.delay_seconds,
