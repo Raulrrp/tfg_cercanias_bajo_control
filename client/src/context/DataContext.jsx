54ae0293d62567c16d235d09fc8bd3e8360cd896
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-06-18 13:09:10 +0200

    refactor: delete React word in the 'react' import

--- client/src/context/DataContext.jsx
+++ client/src/context/DataContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import { createContext, useContext } from 'react';
 import { useStations } from '../hooks/station-hook.js';
 import { useTrains } from '../hooks/train-hook.js';
 import { useRealtimeSnapshot } from '../hooks/realtime-hook.js';
