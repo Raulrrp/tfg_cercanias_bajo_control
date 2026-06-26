698100cfca029277c36bd1335ce0872fa5487393
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-06-26 13:22:53 +0200

    feat: reads the plain DTO array to read departureTime and destination

--- client/src/services/stop-times-service.js
+++ client/src/services/stop-times-service.js
@@ -1,13 +1,10 @@
-import { StopTime } from "@tfg_cercanias_bajo_control/common/models/StopTime.js";
-
 export const fetchStopTimesByStationId = async (stationId) => {
   try {
     const response = await fetch(`${import.meta.env.VITE_API_URL}/stop-times/${stationId}`);
     if (!response.ok) throw new Error(`Failed to fetch upcoming stop times for station ${stationId}`);
 
-    const data = await response.json();
-    // convert data array to domain objects clean of duplicates and past schedules
-    return data.map(stopTimeJson => StopTime.fromJson(stopTimeJson));
+    // return the plain DTO array directly containing departureTime and destination
+    return await response.json();
   } catch (error) {
     console.error('Error fetching stop times from server:', error);
     throw error;
