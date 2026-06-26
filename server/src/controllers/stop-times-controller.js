b7e329fd76c117c9def261f744396c1589ffebc3
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-06-26 12:26:46 +0200

    feat: add getStopTimeByTripIdAndStopId in stop-times-controller and delete getStopTrains

--- server/src/controllers/stop-times-controller.js
+++ server/src/controllers/stop-times-controller.js
@@ -1,26 +1,24 @@
 import * as StopTimesService from '../services/stop-times-service.js';
 
-
-export const getStopTimes = async (req, res) => {
-  try {
-    const stopTimes = await StopTimesService.getStopTimes();
-    res.status(200).json(stopTimes.map(t => t.toJson()));
-  } catch (error) {
-    res.status(500).json({ 
-      message: error.message 
-    });
-  }
-};
-
-
 export const getStopTimesByStopId = async (req, res) => {
   try {
     const { stopId } = req.params;
     const stopTimes = await StopTimesService.getStopTimesByStopId(stopId);
     res.status(200).json(stopTimes.map(t => t.toJson()));
   } catch (error) {
-    res.status(500).json({ 
-      message: error.message 
-    });
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getStopTimeByTripIdAndStopId = async (req, res) => {
+  try {
+    const { tripId, stopId } = req.params;
+    const stopTime = await StopTimesService.getStopTimeByTripIdAndStopId(tripId, stopId);
+    if (!stopTime) {
+      return res.status(404).json({ message: 'Stop time not found for specified trip and stop' });
+    }
+    res.status(200).json(stopTime.toJson());
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
