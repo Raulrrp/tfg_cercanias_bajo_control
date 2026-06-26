74404e39cfcd37de27bc0e80c9853daa1b14abb2
Raul Rueda Perona <RRUEPER@upv.edu.es>
2026-06-26 13:19:27 +0200

    del: remove unused routes in stop-times-routes.js

--- server/src/routes/stop-times-routes.js
+++ server/src/routes/stop-times-routes.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import { getStopTimesByStopId, getStopTimeByTripIdAndStopId } from '../controllers/stop-times-controller.js';
+import { getStopTimesByStopId } from '../controllers/stop-times-controller.js';
 
 const router = express.Router();
 
 // GET /api/stop-times/:stopId - get active stop times for today at a specific stop
 router.get('/:stopId', getStopTimesByStopId);
-
-// GET /api/stop-times/:stopId/trip/:tripId - get specific stop time for a trip at this stop
-router.get('/:stopId/trip/:tripId', getStopTimeByTripIdAndStopId);
 
 export default router;
